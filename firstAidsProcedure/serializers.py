4b20f383216f7a1f8fffbe78b85cf430db349990
sheriefshabaan <95883381+sheriefshabaa@users.noreply.github.com>
2024-08-04 23:54:48 +0300

    - fixing files spacing - Add notes file to the .gitignore file -
    Signed-off-by: sheriefshabaan <sheriefshabaan1@hotmail.com>

--- firstAidsProcedure/serializers.py
+++ firstAidsProcedure/serializers.py
@@ -2,12 +2,8 @@ from rest_framework import serializers
 
 from .models import FirstAidsProcedure
 
+
 class FirstAidsProcedureSerializer(serializers.ModelSerializer):
     class Meta:
         model = FirstAidsProcedure
         fields = '__all__'
-
-
-
-
-
